e04cba7aa22efa764412b4507de39321cd665d6b
Kevin M. Godby <kevin@godby.org>
2017-05-08 16:41:22 -0500

    CMake: install dependency DLLs to osvr_server directory.

--- cmake/FindXimmerse.cmake
+++ cmake/FindXimmerse.cmake
@@ -13,7 +13,6 @@
 # Cache Variables: (probably not for direct use in your scripts)
 #  XIMMERSE_INCLUDE_DIR
 #  XIMMERSE_XDEVICE_LIBRARY
-#  XIMMERSE_INTERPOLATION_LIBRARY
 #
 # Requires these CMake modules:
 #  FindPackageHandleStandardArgs (known included with CMake >=2.6.2)
@@ -31,92 +30,92 @@ set(XIMMERSE_ROOT_DIR
 	PATH
 	"Directory to search for Ximmerse SDK")
 
-set(XIMMERSE_HEADERS_ROOT_DIR
-	"${XIMMERSE_HEADERS_ROOT_DIR}"
-	CACHE
-	PATH
-	"Directory to search for private Ximmerse headers")
-
-set(_root_dirs)
-if(XIMMERSE_ROOT_DIR)
-	set(_root_dirs "${XIMMERSE_ROOT_DIR}" "${XIMMERSE_HEADERS_ROOT_DIR}" "${XIMMERSE_ROOT_DIR}/public")
-endif()
-
-# todo fails for universal builds
-set(_dll_suffix)
-if(${CMAKE_SIZEOF_VOID_P} EQUAL 8)
-	set(_bitness 64)
-	if(WIN32)
-		set(_dll_suffix _x64)
-	endif()
-else()
-	set(_bitness 32)
-endif()
-
-# Test platform
-
-set(_platform)
-if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-	set(_platform_base osx)
-	# SteamVR only supports 32-bit on OS X
-	set(XIMMERSE_PLATFORM osx32)
-else()
-	if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-		set(_platform_base linux)
-		# TODO Massive hack!
-		add_definitions(-DGNUC -DPOSIX -DCOMPILER_GCC -D_LINUX -DLINUX -DPOSIX -D_POSIX)
-	elseif(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
-		set(_platform_base win)
-	endif()
-	set(XIMMERSE_PLATFORM ${_platform_base}${_bitness})
-	set(_libpath lib/${XIMMERSE_PLATFORM})
-endif()
-
 find_path(XIMMERSE_INCLUDE_DIR
 	NAMES
 	xdevice.h
 	HINTS
 	"${CMAKE_CURRENT_SOURCE_DIR}/Vendors/Ximmerse/include"
 	PATHS
-	${_root_dirs})
+	${XIMMERSE_ROOT_DIR})
 
 find_library(XIMMERSE_XDEVICE_LIBRARY
 	NAMES
 	xdevice
 	HINTS
 	"${CMAKE_CURRENT_SOURCE_DIR}/Vendors/Ximmerse/libs"
+	PATHS
+	${XIMMERSE_ROOT_DIR}
 	PATH_SUFFIXES
 	x86
 	x64)
 
-find_library(XIMMERSE_INTERPOLATION_LIBRARY
+find_file(XIMMERSE_WCL04_LIBRARY
 	NAMES
-	interpolation
+	"${CMAKE_SHARED_LIBRARY_PREFIX}wcl04${CMAKE_SHARED_LIBRARY_SUFFIX}"
+	wcl04
 	HINTS
 	"${CMAKE_CURRENT_SOURCE_DIR}/Vendors/Ximmerse/libs"
+	PATHS
+	${XIMMERSE_ROOT_DIR}
 	PATH_SUFFIXES
 	x86
-	x64)
+	x64
+	NO_DEFAULT_PATH)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Ximmerse
 	DEFAULT_MSG
 	XIMMERSE_INCLUDE_DIR
 	XIMMERSE_XDEVICE_LIBRARY
-	XIMMERSE_INTERPOLATION_LIBRARY)
+	XIMMERSE_WCL04_LIBRARY)
 
 if(XIMMERSE_FOUND)
 	list(APPEND XIMMERSE_INCLUDE_DIRS ${XIMMERSE_INCLUDE_DIR})
-	list(APPEND XIMMERSE_LIBRARIES ${XIMMERSE_XDEVICE_LIBRARY} ${XIMMERSE_INTERPOLATION_LIBRARY})
+	list(APPEND XIMMERSE_LIBRARIES ${XIMMERSE_XDEVICE_LIBRARY} ${XIMMERSE_WCL04_LIBRARY})
 	mark_as_advanced(XIMMERSE_ROOT_DIR)
+
+	# Find the associated .dll file
+	if (WIN32)
+		get_filename_component(XIMMERSE_XDEVICE_LIBDIR "${XIMMERSE_XDEVICE_LIBRARY}" PATH)
+		get_filename_component(XIMMERSE_XDEVICE_BASENAME "${XIMMERSE_XDEVICE_LIBRARY}" NAME_WE)
+		get_filename_component(XIMMERSE_XDEVICE_LIBDIR_BASE "${XIMMERSE_XDEVICE_LIBDIR}" PATH)
+		find_file(XIMMERSE_XDEVICE_DLL
+			"${CMAKE_SHARED_LIBRARY_PREFIX}${XIMMERSE_XDEVICE_BASENAME}${CMAKE_SHARED_LIBRARY_SUFFIX}"
+			HINTS
+			"${XIMMERSE_XDEVICE_LIBDIR_BASE}"
+			PATH_SUFFIXES
+			x86
+			x64
+			NO_DEFAULT_PATH
+		)
+		mark_as_advanced(XIMMERSE_XDEVICE_DLL)
+		if(XIMMERSE_XDEVICE_DLL)
+			add_library(Ximmerse::XDevice SHARED IMPORTED)
+			set_property(TARGET Ximmerse::XDevice PROPERTY IMPORTED_IMPLIB "${XIMMERSE_XDEVICE_LIBRARY}")
+			set_property(TARGET Ximmerse::XDevice PROPERTY IMPORTED_LOCATION "${XIMMERSE_XDEVICE_DLL}")
+		else()
+			add_library(Ximmerse::XDevice STATIC IMPORTED)
+			set_property(TARGET Ximmerse::XDevice PROPERTY IMPORTED_LOCATION "${XIMMERSE_XDEVICE_LIBRARY}")
+		endif()
+	else() 
+		add_library(Ximmerse::XDevice UNKNOWN IMPORTED)
+		set_property(TARGET Ximmerse::XDevice PROPERTY IMPORTED_LOCATION "${XIMMERSE_XDEVICE_LIBRARY}")
+	endif()
+	set_property(TARGET Ximmerse::XDevice PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${XIMMERSE_INCLUDE_DIRS}")
+
+	add_library(Ximmerse::WCL04 SHARED IMPORTED)
+	set_property(TARGET Ximmerse::WCL04 PROPERTY IMPORTED_LOCATION "${XIMMERSE_WCL04_LIBRARY}")
+
+	set(XIMMERSE_LIBRARIES Ximmerse::XDevice)# Ximmerse::WCL04)
+	set(XIMMERSE_INCLUDE_DIRS "${XIMMERSE_INCLUDE_DIR}")
+
+	add_library(Ximmerse::Ximmerse INTERFACE IMPORTED)
+	set_target_properties(Ximmerse::Ximmerse
+		PROPERTIES
+		INTERFACE_LINK_LIBRARIES "${XIMMERSE_LIBRARIES}"
+		INTERFACE_INCLUDE_DIRECTORIES "${XIMMERSE_INCLUDE_DIRS}")
 endif()
 
-mark_as_advanced(XIMMERSE_INCLUDE_DIR XIMMERSE_XDEVICE_LIBRARY XIMMERSE_INTERPOLATION_LIBRARY)
+mark_as_advanced(XIMMERSE_INCLUDE_DIR XIMMERSE_XDEVICE_LIBRARY XIMMERSE_WCL04_LIBRARY)
 
-if(Ximmerse_FOUND AND NOT TARGET Ximmerse::Ximmerse)
-	add_library(Ximmerse::Ximmerse UNKNOWN IMPORTED)
-	set_target_properties(Ximmerse::Ximmerse PROPERTIES
-		IMPORTED_LOCATION "${Ximmerse_LIBRARIES}"
-		INTERFACE_INCLUDE_DIRECTORIES "${Ximmerse_INCLUDE_DIRS}")
-endif()
 
